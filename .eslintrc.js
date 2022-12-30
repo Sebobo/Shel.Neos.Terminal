65265ec88331e6c96925c02d7a9d8c07b5116414
Sebastian Helzle <sebastian@helzle.it>
2022-12-30 13:52:45 +0100

    TASK: Cleanup dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,8 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
         'plugin:prettier/recommended',
-        'prettier/@typescript-eslint',
     ],
-    plugins: ['prettier', 'react', 'react-hooks'],
+    plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks'],
     settings: {
         react: {
             version: 'detect',
