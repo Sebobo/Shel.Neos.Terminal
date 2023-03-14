2d188e735557b008a3bae3ac63d219a83d7e0114
Sebastian Helzle <sebastian@helzle.it>
2023-03-14 10:35:16 +0700

    TASK: Use get request for retrieving commands

--- Resources/Private/JavaScript/Terminal/src/helpers/fetchCommands.ts
+++ Resources/Private/JavaScript/Terminal/src/helpers/fetchCommands.ts
@@ -1,11 +1,12 @@
 // @ts-ignore
 import { fetchWithErrorHandling } from '@neos-project/neos-ui-backend-connector';
+import { CommandList } from '../interfaces';
 
-const fetchCommands = async (endPoint) => {
+const fetchCommands = async (endPoint): Promise<{ success: boolean; result: CommandList }> => {
     return fetchWithErrorHandling
         .withCsrfToken((csrfToken) => ({
             url: endPoint,
-            method: 'POST',
+            method: 'GET',
             credentials: 'include',
             headers: {
                 'X-Flow-Csrftoken': csrfToken,
