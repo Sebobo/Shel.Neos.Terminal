77c40192f466273fe399a0ed0f6b0178e044825c
Markus Günther <info@unikka.de>
2024-01-03 18:46:36 +0100

    BUGFIX: Prevent json encode error

--- Resources/Private/JavaScript/Terminal/src/helpers/fetchCommands.ts
+++ Resources/Private/JavaScript/Terminal/src/helpers/fetchCommands.ts
@@ -3,17 +3,29 @@ import { fetchWithErrorHandling } from '@neos-project/neos-ui-backend-connector'
 import { CommandList } from '../interfaces';
 
 const fetchCommands = async (endPoint): Promise<{ success: boolean; result: CommandList }> => {
-    return fetchWithErrorHandling
-        .withCsrfToken((csrfToken) => ({
-            url: endPoint,
-            method: 'GET',
-            credentials: 'include',
-            headers: {
-                'X-Flow-Csrftoken': csrfToken,
-                'Content-Type': 'application/json',
-            },
-        }))
-        .then((response) => response && response.json());
+    try {
+        const response = await fetchWithErrorHandling
+            .withCsrfToken((csrfToken) => ({
+                url: endPoint,
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'X-Flow-Csrftoken': csrfToken,
+                    'Content-Type': 'application/json',
+                },
+            }));
+
+        if (response.status === 403) {
+            // Handle 403 errors here
+            return { success: false, result: {} };
+        }
+
+        const result = await response.json();
+        return { success: true, result };
+    } catch (error) {
+        // Handle other errors here
+        return { success: false, result: {} };
+    }
 };
 
 export default fetchCommands;
