c3c6ec7dae30d744a414a3f1f924e2521cdb2c7b
Runa Schlör <cr.schloer@posteo.de>
2024-03-15 00:32:06 +0100

    TMP

--- Classes/Command/SearchCommand.php
+++ Classes/Command/SearchCommand.php
@@ -13,12 +13,30 @@ namespace Shel\Neos\Terminal\Command;
  * source code.
  */
 
+use GuzzleHttp\Psr7\Uri;
+use http\Env\Request;
+use http\Message;
+use Neos\Cache\Exception;
+use Neos\ContentRepository\Core\Projection\ContentGraph\Filter\FindDescendantNodesFilter;
+use Neos\Flow\Mvc\Routing\Dto\ResolveContext;
+use Neos\Flow\Mvc\Routing\Dto\RouteParameters;
+use Neos\Neos\FrontendRouting\EventSourcedFrontendNodeRoutePartHandler;
+use Neos\Neos\FrontendRouting\Projection\DocumentUriPathFinder;
+use ProxyManager\Exception\ExceptionInterface;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Neos\ContentGraph\DoctrineDbalAdapter\Domain\Repository\ContentSubgraph;
+use Neos\ContentRepository\Core\ContentRepository;
+use Neos\ContentRepository\Core\Projection\ContentGraph\Filter\FindAncestorNodesFilter;
+use Neos\ContentRepository\Core\Projection\ContentGraph\Filter\FindChildNodesFilter;
+use Neos\ContentRepository\Core\Projection\ContentGraph\Filter\NodeType\NodeTypeCriteria;
 use Neos\ContentRepository\Core\Projection\ContentGraph\Node;
+use Neos\ContentRepository\Core\Projection\ContentGraph\Nodes;
+use Neos\ContentRepository\Core\Projection\ContentGraph\VisibilityConstraints;
+use Neos\ContentRepositoryRegistry\ContentRepositoryRegistry;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Cache\CacheManager;
 use Neos\Flow\I18n\Translator;
 use Neos\Flow\Mvc\Controller\ControllerContext;
-use Neos\Neos\Domain\Service\NodeSearchServiceInterface;
 use Neos\Neos\Service\LinkingService;
 use Shel\Neos\Terminal\Domain\CommandContext;
 use Shel\Neos\Terminal\Domain\CommandInvocationResult;
@@ -51,9 +69,15 @@ class SearchCommand implements TerminalCommandInterface
 
     /**
      * @Flow\Inject
-     * @var NodeSearchServiceInterface
+     * @var ContentRepositoryRegistry
      */
-    protected $nodeSearchService;
+    protected $contentRepositoryRegistry;
+
+    /**
+     * @Flow\Inject
+     * @var EventSourcedFrontendNodeRoutePartHandler
+     */
+    protected $frontendNodeRoutePartHandler;
 
     public static function getCommandName(): string
     {
@@ -112,29 +136,41 @@ class SearchCommand implements TerminalCommandInterface
                     'Shel.Neos.Terminal'));
         }
 
-        // The NodeSearchInterface does not yet have a 4th argument for the startingPoint but all known implementations do
-        $nodes = $this->nodeSearchService->findByProperties(
-            $input->getArgument('searchword'),
-            $input->getOption('nodeTypes'),
-            $contextNode->getContext(),
-            $contextNode
+        /** @var ContentSubgraph $subgraph */
+        $subgraph = $this->contentRepositoryRegistry->subgraphForNode(($contextNode));
+
+        try{
+            $searchedNodeTypes = $input->getArgument('nodeTypes');
+        }catch (InvalidArgumentException $e){
+            $searchedNodeTypes = null;
+        }
+
+        $nodes = iterator_to_array($subgraph->findDescendantNodes(
+            $contextNode->nodeAggregateId,
+            FindDescendantNodesFilter::create(nodeTypes: $searchedNodeTypes, searchTerm: $input->getArgument('searchword')))
         );
 
-        $results = array_map(function ($node) use ($documentNode, $commandContext) {
-            $breadcrumbs = [];
-            $parent = $node->getParent();
-            while ($parent) {
-                if ($parent->getNodeType()->isOfType('Neos.Neos:Document')) {
-                    $breadcrumbs[] = $parent->getLabel();
-                }
-                $parent = $parent->getParent();
+        // todo get live uri
+        // probably use UriBuilder Packages/Framework/Neos.Flow/Classes/Mvc/Routing/UriBuilder.php
+        //$documentUriPathFinder = $this->contentRepositoryRegistry->projectionState(DocumentUriPathFinder::class);
+        //$documentUriPathFinder-> getLiveContentStreamId
+        //or
+        // DynamicRoutePart::resolveWithParameters
+
+        $results = array_map(function ($node) use ($documentNode, $commandContext, $subgraph, $contextNode) {
+
+            try{
+                $nodePath = $subgraph->retrieveNodePath($node->nodeAggregateId);
+            }catch(\InvalidArgumentException $e){
+                $nodePath = "NodePath could not be retrieved: $e";
             }
 
             return [
                 'label' => $node->getLabel(),
-                'nodeType' => $node->getNodeType()->getName(),
-                'breadcrumb' => implode(' / ', array_reverse($breadcrumbs)),
+                'nodeType' => $node->nodeTypeName,
+                'nodePath' => $nodePath,
                 'uri' => $this->getUriForNode($commandContext->getControllerContext(), $documentNode, $documentNode),
+                'tst' => $this->getUriForNode(ControllerContext(new Request(), new Res))
             ];
         }, $nodes);
 
@@ -143,9 +179,10 @@ class SearchCommand implements TerminalCommandInterface
 
     protected function getUriForNode(
         ControllerContext $controllerContext,
-        Node $node,
-        Node $baseNode
-    ): string {
+        Node              $node,
+        Node              $baseNode
+    ): string
+    {
         try {
             return $this->linkingService->createNodeUri(
                 $controllerContext,
